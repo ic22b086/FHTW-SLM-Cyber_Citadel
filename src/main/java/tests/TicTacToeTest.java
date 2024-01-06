fbfcd96e0528e1ce58c4420c4b00d3af0b897edb
Johannes Seitz <ic22b086@technikum-wien.at>
2024-01-06 17:49:06 +0100

    added test for validate move

--- src/main/java/tests/TicTacToeTest.java
+++ src/main/java/tests/TicTacToeTest.java
@@ -3,10 +3,29 @@ package tests;
 // TicTacToeTest.java
 import main.Player;
 import main.TicTacToe;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp(){
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void closeStream(){
+        System.setOut(standardOut);
+    }
 
     @Test
     public void testSwitchCurrentPlayer() {
@@ -33,6 +52,26 @@ public class TicTacToeTest {
         game.getBoard().place(0, 2, 'X');
 
         assertTrue(game.hasWinner());
+    }
+
+
+    @Test
+    public void testValidateMove(){
+        Player playerX = new Player('X');
+        Player playerO = new Player('O');
+        TicTacToe game = new TicTacToe(playerX, playerO);
+
+        game.getBoard().place(0, 0, 'X');
+        game.getBoard().place(0, 0, 'X');
+
+        assertEquals("This cell is already taken, Please choose another cell", outputStream.toString());
+
+        game.getBoard().place(4, 4, 'X');
+        assertEquals("Invalid row. Please enter a number between 0 and 2.", outputStream.toString());
+
+        game.getBoard().place(0, 4, 'X');
+        assertEquals("Invalid column. Please enter a number between 0 and 2.", outputStream.toString());
+
     }
 
     // todo add further tests
