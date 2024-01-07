8e35355d7991b96ea77c577c9bbae8251cf2d144
Johannes Seitz <ic22b086@technikum-wien.at>
2024-01-07 13:04:07 +0100

    fixed validate move tests, improved board full and assert winner tests, added player to string test

--- src/main/java/tests/PlayerTest.java
+++ src/main/java/tests/PlayerTest.java
@@ -2,14 +2,34 @@ package tests;
 
 // PlayerTest.java
 import main.Player;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp(){
+        System.setOut(new PrintStream(outputStream));
+    }
 
     @Test
     public void testGetMarker() {
         Player player = new Player('X');
         assertEquals('X', player.getMarker());
+    }
+
+    @Test
+    public void testPlayertoString(){
+        Player player = new Player('X');
+        assertEquals("X", player.toString());
+
     }
 }
--- src/main/java/tests/TicTacToeTest.java
+++ src/main/java/tests/TicTacToeTest.java
@@ -3,10 +3,29 @@ package tests;
 // TicTacToeTest.java
 import main.Player;
 import main.TicTacToe;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
+
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp(){
+        System.setOut(new PrintStream(outputStream));
+    }
+
+    @AfterEach
+    public void closeStream(){
+        System.setOut(standardOut);
+    }
 
     @Test
     public void testSwitchCurrentPlayer() {
@@ -33,6 +52,55 @@ public class TicTacToeTest {
         game.getBoard().place(0, 2, 'X');
 
         assertTrue(game.hasWinner());
+
+
+        // diagonal
+
+        game.getBoard().clear();
+
+        game.getBoard().place(0, 0, 'X');
+        game.getBoard().place(1, 1, 'X');
+        game.getBoard().place(2, 2, 'X');
+
+        assertTrue(game.hasWinner());
+
+
+        //Full and no winner:
+
+        game.getBoard().clear();
+
+        game.getBoard().place(0, 0, 'X');
+        game.getBoard().place(0, 1, 'O');
+        game.getBoard().place(0, 2, 'X');
+        game.getBoard().place(1, 0, 'O');
+        game.getBoard().place(1, 1, 'O');
+        game.getBoard().place(1, 2, 'X');
+        game.getBoard().place(2, 0, 'X');
+        game.getBoard().place(2, 1, 'X');
+        game.getBoard().place(2, 2, 'O');
+
+        assertFalse(game.hasWinner());
+
+    }
+
+
+    @Test
+    public void testValidateMove(){
+        Player playerX = new Player('X');
+        Player playerO = new Player('O');
+        TicTacToe game = new TicTacToe(playerX, playerO);
+
+        game.makeMove(0, 0);
+        game.makeMove(0, 0);
+
+        assertEquals("This cell is already taken, Please choose another cell\n", outputStream.toString());
+
+        game.makeMove(4,4);
+        assertEquals("Invalid row. Please enter a number between 0 and 2.\n", outputStream.toString());
+
+        game.makeMove(0,4);
+        assertEquals("Invalid column. Please enter a number between 0 and 2.\n", outputStream.toString());
+
     }
 
     // todo add further tests
