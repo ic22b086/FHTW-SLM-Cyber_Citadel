024d01c0ee72b0f45d4b1522cc88d4aea400f4f7
ic22b501 <ic22b501@technikum-wien.at>
2024-01-07 18:41:47 +0100

    initial

--- src/main/java/tests/BoardTest.java
+++ src/main/java/tests/BoardTest.java
@@ -13,7 +13,7 @@ public class BoardTest {
 
         assertTrue(board.isCellEmpty(0, 0));
         board.place(0, 0, 'X');
-        assertFalse(board.isCellEmpty(0, 0));
+        assertFalse(board.isCellEmpty(0,0));
     }
 
     @Test
@@ -25,7 +25,56 @@ public class BoardTest {
 
         assertTrue(board.isFull());
     }
+    @Test
+    public void testClear() {
+        Board board = new Board();
 
-    // todo implement further game logic
+        //Place test markler
+        board.place(0,0, 'X');
+        board.place(1, 1, 'O');
+
+            //Clear board
+        board.clear();
+
+        //Check if clearing succeeded
+        assertTrue(board.isCellEmpty(0,0));
+        assertTrue(board.isCellEmpty(1, 1));
+    }
+
+    @Test
+    public void testPlaceAndGet() {
+        Board board = new Board();
+
+        //PLace initial markers
+        board.place(0,0, 'X');
+        board.place(1, 1, 'O');
+        board.place(2,2,'X');
+
+            //Check if markers are correctly read
+        assertEquals('X', board.getCell(0,0));
+        assertEquals('O', board.getCell(1, 1));
+        assertEquals('X', board.getCell(2,2));
+    }
+
+    @Test
+    public void testInvalidPlacement() {
+        Board board = new Board();
+
+                    //Place initial marker
+        assertTrue(board.place(0, 0, 'X'));
+
+        //try replacing Marker
+        assertFalse(board.place(0, 0, 'O'));
+
+            //testing out of bounds
+        assertFalse(board.place(3, 3, 'O'));
+
+        //Check for bioard changes
+        assertEquals('X', board.getCell(0, 0));
+        assertEquals(' ', board.getCell(1, 1));
+    }
+
 }
+
+
 
