674d73be61e2eb08d591930f96cff976d45bd6bf
ic22b086 <129212625+ic22b086@users.noreply.github.com>
2024-01-07 19:26:01 +0100

    Update TicTacToeTest.java
    Cleaned up imports

--- src/main/java/tests/TicTacToeTest.java
+++ src/main/java/tests/TicTacToeTest.java
@@ -4,16 +4,13 @@ package tests;
 import main.Player;
 import main.TicTacToe;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 
-import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
 import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
 
 public class TicTacToeTest {
 
