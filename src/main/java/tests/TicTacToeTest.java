d5a2f19cc4c55721aa766b55fc43dbeef0775c9f
ic22b086 <129212625+ic22b086@users.noreply.github.com>
2024-01-07 18:23:43 +0100

    Update TicTacToeTest.java
    Removed static in import org.junit.jupiter.api.Assertions for Pull request

--- src/main/java/tests/TicTacToeTest.java
+++ src/main/java/tests/TicTacToeTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
 
